6d4273f0f0b4a535afa8768f377054dc1de3a604
vellorea5 <vellore.a5@gmail.com>
2023-08-26 12:28:31 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<p>Hello Angular</p>',
+  //templateUrl: './app.component.html',
+  //styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'HelloNG';
+  //title = 'HelloNG';
 }
